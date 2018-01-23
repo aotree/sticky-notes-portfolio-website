481576bb6f26cbc30304f742fef822b3901676d6
aotree <aotree423@gmail.com>
2018-01-23 11:45:26 +0900

    Add files.
    Add files.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function() {
+	'user strict';
+
+	var programming = document.getElementById('programming');
+	programming.addEventListener('click', function() {
+		window.open("https://github.com/aotree");
+	});
+
+	var blog = document.getElementById('blog');
+	blog.addEventListener('click', function() {
+		window.open("http://aokihiroki.hatenablog.com/");
+	});
+
+	var music = document.getElementById('music');
+	music.addEventListener('click', function() {
+		window.open("http://mauveevening.wixsite.com/official");
+	});
+
+	var illustration = document.getElementById('illustration');
+	illustration.addEventListener('click', function() {
+		window.open("https://note.mu/aokihiroki");
+	});
+})();
